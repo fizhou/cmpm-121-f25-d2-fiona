fde37bf87a60b9a0efcec8bb0c79b51cd601e903
Fiona Zhou <fizhou@ucsc.edu>
2025-10-17 02:15:43 +0000

    removed icon and associated code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,1 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
-
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
